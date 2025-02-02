9b1149addc462a8cfd25b88696b605edc5bb6934
nitou-kanazawa <121334451+nitou-kanazawa@users.noreply.github.com>
2025-02-02 20:21:49 +0900

    各種更新．

--- src/RPG.Domain/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace RPG.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/RPG.Domain/UserPreferences.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace RPG.Domain.Configs {
+
+    public class UserPreferences {
+
+    }
+
+}
